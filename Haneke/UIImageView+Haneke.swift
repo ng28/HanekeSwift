bb967303380c8e82b1f90f36ed998d58aeccbfa1
nagendhiran.r <nagendhiran.r@gmail.com>
2017-11-08 10:09:59 +0530

    - Allow Haneke to load on zero sized uiimage frame.

--- Haneke/UIImageView+Haneke.swift
+++ Haneke/UIImageView+Haneke.swift
@@ -9,29 +9,37 @@
 import UIKit
 
 public extension UIImageView {
-    
+
     public var hnk_format : Format<UIImage> {
         let viewSize = self.bounds.size
             assert(viewSize.width > 0 && viewSize.height > 0, "[\(Mirror(reflecting: self).description) \(#function)]: UImageView size is zero. Set its frame, call sizeToFit or force layout first.")
             let scaleMode = self.hnk_scaleMode
             return HanekeGlobals.UIKit.formatWithSize(viewSize, scaleMode: scaleMode)
     }
-    
+
     public func hnk_setImageFromURL(_ URL: Foundation.URL, placeholder : UIImage? = nil, format : Format<UIImage>? = nil, failure fail : ((Error?) -> ())? = nil, success succeed : ((UIImage) -> ())? = nil) {
         let fetcher = NetworkFetcher<UIImage>(URL: URL)
         self.hnk_setImage(fromFetcher: fetcher, placeholder: placeholder, format: format, failure: fail, success: succeed)
     }
-    
+
+    public func hnk_setImageFromURLAutoSize(_ url: Foundation.URL) {
+        var format: Format<UIImage>? = nil
+        if frame.size == CGSize.zero {
+            format = Format<UIImage>(name: "original")
+        }
+        hnk_setImageFromURL(url, format: format)
+    }
+
     public func hnk_setImage( _ image: @autoclosure @escaping () -> UIImage, key: String, placeholder : UIImage? = nil, format : Format<UIImage>? = nil, success succeed : ((UIImage) -> ())? = nil) {
         let fetcher = SimpleFetcher<UIImage>(key: key, value: image)
         self.hnk_setImage(fromFetcher: fetcher, placeholder: placeholder, format: format, success: succeed)
     }
-    
+
     public func hnk_setImageFromFile(_ path: String, placeholder : UIImage? = nil, format : Format<UIImage>? = nil, failure fail : ((Error?) -> ())? = nil, success succeed : ((UIImage) -> ())? = nil) {
         let fetcher = DiskFetcher<UIImage>(path: path)
         self.hnk_setImage(fromFetcher: fetcher, placeholder: placeholder, format: format, failure: fail, success: succeed)
     }
-    
+
     public func hnk_setImage(fromFetcher fetcher : Fetcher<UIImage>,
         placeholder : UIImage? = nil,
         format : Format<UIImage>? = nil,
@@ -39,27 +47,27 @@ public extension UIImageView {
         success succeed : ((UIImage) -> ())? = nil) {
 
         self.hnk_cancelSetImage()
-        
+
         self.hnk_fetcher = fetcher
-        
+
         let didSetImage = self.hnk_fetchImageForFetcher(fetcher, format: format, failure: fail, success: succeed)
-        
+
         if didSetImage { return }
-     
+
         if let placeholder = placeholder {
             self.image = placeholder
         }
     }
-    
+
     public func hnk_cancelSetImage() {
         if let fetcher = self.hnk_fetcher {
             fetcher.cancelFetch()
             self.hnk_fetcher = nil
         }
     }
-    
+
     // MARK: Internal
-    
+
     // See: http://stackoverflow.com/questions/25907421/associating-swift-things-with-nsobject-instances
     var hnk_fetcher : Fetcher<UIImage>! {
         get {
@@ -75,7 +83,7 @@ public extension UIImageView {
             objc_setAssociatedObject(self, &HanekeGlobals.UIKit.SetImageFetcherKey, wrapper, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
     }
-    
+
     public var hnk_scaleMode : ImageResizer.ScaleMode {
         switch (self.contentMode) {
         case .scaleToFill:
@@ -99,25 +107,25 @@ public extension UIImageView {
         let fetch = cache.fetch(fetcher: fetcher, formatName: format.name, failure: {[weak self] error in
             if let strongSelf = self {
                 if strongSelf.hnk_shouldCancel(forKey: fetcher.key) { return }
-                
+
                 strongSelf.hnk_fetcher = nil
-                
+
                 fail?(error)
             }
         }) { [weak self] image in
             if let strongSelf = self {
                 if strongSelf.hnk_shouldCancel(forKey: fetcher.key) { return }
-                
+
                 strongSelf.hnk_setImage(image, animated: animated, success: succeed)
             }
         }
         animated = true
         return fetch.hasSucceeded
     }
-    
+
     func hnk_setImage(_ image : UIImage, animated : Bool, success succeed : ((UIImage) -> ())?) {
         self.hnk_fetcher = nil
-        
+
         if let succeed = succeed {
             succeed(image)
         } else if animated {
@@ -128,12 +136,12 @@ public extension UIImageView {
             self.image = image
         }
     }
-    
+
     func hnk_shouldCancel(forKey key:String) -> Bool {
         if self.hnk_fetcher?.key == key { return false }
-        
+
         Log.debug(message: "Cancelled set image for \((key as NSString).lastPathComponent)")
         return true
     }
-    
+
 }
